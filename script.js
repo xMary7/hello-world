00c8d8d87f1bf9a37115de72656c60234ccb7dd7
xMary7 <aerodromnaya.70@mail.ru>
2015-05-13 14:38:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,6 @@ var startGame = function() {
      }
        //$('.card').addClass('cardOut');
        //$('.layer').css('z-index', -1);
-<<<<<<< HEAD
      }
 
 var gamePlay = function () {
@@ -50,46 +49,18 @@ var gamePlay = function () {
       })
   });
 };
-=======
-	   }
-
-var gamePlay = function() {
- 	    $('#card0').on("mouseenter mouseleave", function(){
-	console.log('clicked');
-	 //alert('#card0');
-
-    //var clickX = (event.layerX == undefined ? event.offsetX : event.layerX) + 1;
-
-    //var clickY = (event.layerY == undefined ? event.offsetY : event.layerY) + 1;
-
-    //alert('Координаты клика: '+ clickX +' x '+ clickY);
-
-  
-   } );
- }
->>>>>>> origin/master
 
 
 
 $(document).ready(function() {
-<<<<<<< HEAD
   console.log(arrayFromHtml);
-=======
-	console.log(arrayFromHtml);
->>>>>>> origin/master
 
    $('#NG').click(function(){
 
           $('#g').empty();
           $('#NewGame').css('z-index', -2);
-<<<<<<< HEAD
       startGame();
       gamePlay();
       
-=======
-		  startGame();
-		  gamePlay();
-		  
->>>>>>> origin/master
    });  
 });
